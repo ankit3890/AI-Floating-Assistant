6ed0708aa7c4252a13981d8dbf21d1eb8fb24ab0
ankit3890 <ankitidea3890@gmail.com>
2026-01-25 00:53:07 +0530

    chore: remove duplicate capture files from root

--- capture-preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-    onShowCapture: (callback) => ipcRenderer.on('capture-init', (event, dataUrl) => callback(dataUrl)),
-    completeCapture: (dataUrl) => ipcRenderer.send('capture-complete', dataUrl),
-    cancelCapture: () => ipcRenderer.send('capture-cancel')
-});
